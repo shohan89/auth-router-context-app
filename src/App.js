1d0b6aa14f5412abe2c40e1d9888035f40c2c876
shoh4n <maffin.killer007@gmail.com>
2023-08-26 13:45:30 +0600

    Header and nav menu added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 import './App.css';
+import Main from './components/layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/login',
+          element: <Login />
+        },
+        {
+          path: '/register',
+          element: <Register />
+        }
+      ]
+    }
+  ]);
   return (
-    <div className="App">
-      
+    <div className="app">
+      <RouterProvider router={ router } />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h2>This is registration</h2>
+        </div>
+    );
+};
+
+export default Register;
