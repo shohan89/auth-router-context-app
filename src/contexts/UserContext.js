56ece0445f3735f747b7ea2c3ca3aed561794775
shoh4n <maffin.killer007@gmail.com>
2023-09-05 16:56:03 +0600

    Use onAuthStateChanged to manage user auth state

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 
@@ -16,6 +16,16 @@ const UserContext = ({ children }) => {
     const signIn = ( email, password ) =>{
         return signInWithEmailAndPassword( auth, email, password );
     }
+
+    // why we are doing this
+    useEffect( ()=>{
+        const unsubscribe = onAuthStateChanged( auth, currentUser => {
+            setUser( currentUser );
+        })
+        return () =>{
+            unsubscribe();
+        }
+    }, [])
 
     const authInfo = { user, createUser, signIn };
     return (
